953c2e1b75c4c394c5c43ce289a4576f5650fe99
Jesús Luna <jes24.lu@gmail.com>
2026-09-23 11:20:37 -0600

    implementación_sort

--- insertion_sort.cpp
+++ insertion_sort.cpp
@@ -1,23 +1,49 @@
 #include <iostream>
-using namespace std;
 
-void mostrarArreglo(int numeros[], int n)
-{
-    for (int i = 0; i < n; i++)
-    {
-        cout << numeros[i] << " ";
+void mostrarArreglo(const int numeros[], int n) {
+    for (int i = 0; i < n; ++i) {
+        std::cout << numeros[i] << " ";
     }
-
-    cout << endl;
+    std::cout << '\n';
 }
 
-int main()
-{
-    int numeros[] = {7, 3, 8, 2, 6, 4, 5};
-    int n = 7;
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
 
-    cout << "Arreglo original:" << endl;
+    int numeros[] = {7, 3, 8, 2, 6, 4, 5};
+    constexpr int n = 7;
+
+    std::cout << "Arreglo original:\n";
     mostrarArreglo(numeros, n);
+
+    int comparaciones = 0, desplazamientos = 0, inserciones = 0;
+
+    for (int i = 1; i < n; ++i) {
+        int key = numeros[i];
+        int j = i - 1;
+
+        std::cout << "\nInsertando: " << key << '\n';
+
+        for (; j >= 0; --j) {
+            ++comparaciones;
+            if (numeros[j] <= key) break;
+            numeros[j + 1] = numeros[j];
+            ++desplazamientos;
+        }
+
+        numeros[j + 1] = key;
+        ++inserciones;
+
+        mostrarArreglo(numeros, n);
+    }
+
+    std::cout << "\n========================================\n"
+            << "       ESTADISTICAS\n"
+            << "========================================\n"
+            << "Comparaciones: " << comparaciones << '\n'
+            << "Desplazamientos: " << desplazamientos << '\n'
+            << "Inserciones: " << inserciones << '\n';
 
     return 0;
 }
